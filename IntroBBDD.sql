b4f55c08c16a676724014621fe7129c7ba5bfe8a
Guillermo Fuster <gravity606@hotmail.com>
2024-04-23 12:08:51 +0200

    Create IntroBBDD.sql
    Ejercicios de introduccion a BBDD sql

---
+++ IntroBBDD.sql
@@ -0,0 +1,9 @@
+SELECT flight_id, flight_no, status FROM flights WHERE status = 'On Time' ;
+
+SELECT * FROM bookings WHERE total_amount >= 1000000 ;
+
+SELECT * FROM aircraft_data ;
+
+SELECT flight_id, flight_no, FROM flights WHERE aircraft_code = '737' ;
+
+SELECT * FROM tickets WHERE passenger_name LIKE 'IRINA%' ;
